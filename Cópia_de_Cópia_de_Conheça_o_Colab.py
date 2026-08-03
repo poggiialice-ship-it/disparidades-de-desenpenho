3e863d3303d23eb7f431a37862f715700d04ff47
poggiialice-ship-it <poggiialice@gmail.com>
2026-08-03 20:25:41 -0300

    Update Cópia_de_Cópia_de_Conheça_o_Colab.py

--- Cópia_de_Cópia_de_Conheça_o_Colab.py
+++ Cópia_de_Cópia_de_Conheça_o_Colab.py
@@ -937,7 +937,7 @@
     },
     {
       "cell_type": "code",
-      "execution_count": null,
+      "execution_count": None,
       "metadata": {
         "colab": {
           "base_uri": "https://localhost:8080/",
